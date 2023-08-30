3be7091d726f5f8dba51a5c4b5b9fb11e9e65739
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2023-08-30 21:13:49 +0300

    classcard

--- js/script.js
+++ js/script.js
@@ -144,7 +144,7 @@ document.addEventListener('keydown', (e) => {
 });//(событие 'keydown' вешается на документ и означает нажатие клавиши на клавиатуре)
 
 //(чтобы модальное окно открывалось, когда пользователь долистает до конца страницы и через несколько секунд задержки)
-const modalTimerId = setTimeout(openModal, 5000);
+const modalTimerId = setTimeout(openModal, 5000);//закоментировать,чтобы не выскакивало
 
 function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {//(как только пользователь долистал до конца - 1 пиксель)
@@ -155,4 +155,68 @@ function showModalByScroll() {
 
 window.addEventListener('scroll', showModalByScroll);
 //====================================================
+//=========== ИСПОЛЬЗУЕМ КЛАССЫ ДЛЯ КАРТОЧЕК  =======
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH(); 
+        }
+
+        changeToUAH() {//(конвертация из доллара в гривню)
+            this.price = this.price * this.transfer; 
+        }
+
+        render() {
+            const element = document.createElement('div');//(создаем див)
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;//(создаем структуру)
+            this.parent.append(element);//(помещаем на страницу)
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес New"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное New"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум New”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();//(создаем объект, сразу на него на месте вызываем метод render. Если таким синтаксисом создавать его, то после того, как он отработает, он исчезнет, потому что мы нигде не сохраняем на него ссылок. Это удобно, когда нужно только одн раз его использовать)
+
+//========================================================
 });
