7a4c1f342f5b535e028ffeb546affe432d95cc38
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2023-08-31 10:04:51 +0300

    rest

--- js/script.js
+++ js/script.js
@@ -158,12 +158,13 @@ window.addEventListener('scroll', showModalByScroll);
 //=========== ИСПОЛЬЗУЕМ КЛАССЫ ДЛЯ КАРТОЧЕК  =======
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;//(rest оператор = массив)
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH(); 
@@ -175,17 +176,23 @@ window.addEventListener('scroll', showModalByScroll);
 
         render() {
             const element = document.createElement('div');//(создаем див)
-            element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
-                </div>
+
+            if (this.classes.length === 0) {//(если в массив с классами не передался не один класс, то перезаписываем в массив дефолтный класс menu__item и добавляем его элементу)
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            
+            element.innerHTML = `                
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>                
             `;//(создаем структуру)
             this.parent.append(element);//(помещаем на страницу)
         }
@@ -198,6 +205,7 @@ window.addEventListener('scroll', showModalByScroll);
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
         ".menu .container"
+        //'menu__item'//(пишем без точки впереди, потому что будет помещено в массив)
     ).render();
 
     new MenuCard(
@@ -217,6 +225,10 @@ window.addEventListener('scroll', showModalByScroll);
         21,
         ".menu .container"
     ).render();//(создаем объект, сразу на него на месте вызываем метод render. Если таким синтаксисом создавать его, то после того, как он отработает, он исчезнет, потому что мы нигде не сохраняем на него ссылок. Это удобно, когда нужно только одн раз его использовать)
-
 //========================================================
 });
+
+
+
+
+
